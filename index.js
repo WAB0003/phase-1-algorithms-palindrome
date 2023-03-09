0f1f022391cea54e04d375b613c228901e99dac0
Bill Brown <bill.a.brown90@gmail.com>
2023-03-09 13:54:32 -0800

    practicing algorithms

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  //expect take each letter of the word provided and create a new word with those letters in the reverse order.
+  //compare that new word with the old word. If they are the same, then the answer is true, and the word is a Palindrome.
+
+  let wordBackwards = ""
+  for (i=word.length-1;i>=0;i--){
+  let letter = word.charAt(i)
+  wordBackwards += letter
+  }
+return wordBackwards===word
 }
 
 /* 
   Add your pseudocode here
+  create a new word called wordBackwards and set it to a blank string
+
+  usering a for loop, take whatever word is given and iterate in reverse. 
+    take each letter provided and add it to the empty string of "wordBackwards"
+
+  compare "wordBackwards" to "word". return result!
 */
 
 /*
@@ -23,3 +38,5 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
+
